f231c53725e296d3764a2a6067187be05b83294c
Akapaprika <oxygen149656@gmail.com>
2026-03-14 09:57:49 +0900

    build: ビルドモードの削除
    - 環境固有のパスを使わないよう変更
    - RelWithDebInfoモードのビルド設定削除

---
+++ native/build.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+REM =====================================
+REM Build script for PuyotanNative
+REM Usage:
+REM   build.bat        -> Release build
+REM   build.bat -d     -> Debug build
+REM =====================================
+
+set MODE=Release
+
+if "%1"=="-d" set MODE=Debug
+
+echo Build mode: %MODE%
+
+set BUILD_DIR=build_%MODE%
+
+echo === Configure (%MODE%) ===
+REM Pythonからpybind11のCMakeディレクトリを動的に取得する
+FOR /F "tokens=*" %%i IN ('python -c "import pybind11; print(pybind11.get_cmake_dir().replace('\\', '/'))"') DO set PYBIND11_CMAKE_DIR=%%i
+
+cmake -S . -B %BUILD_DIR% ^
+    -Dpybind11_DIR="%PYBIND11_CMAKE_DIR%"
+
+echo === Build (%MODE%) ===
+cmake --build %BUILD_DIR% --config %MODE% --parallel
+
+echo === Done (%MODE%) ===
+
+endlocal
