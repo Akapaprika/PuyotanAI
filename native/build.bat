1d4ec2d92b03e4a439132b72f856460ab2cc5864
Akapaprika <oxygen149656@gmail.com>
2026-04-03 19:43:35 +0900

    chore: ビルドのバッチファイルがどこからでも実行できるように変更

--- native/build.bat
+++ native/build.bat
@@ -14,17 +14,17 @@ if "%1"=="-d" set MODE=Debug
 
 echo Build mode: %MODE%
 
-set BUILD_DIR=build_%MODE%
+set BUILD_DIR=%~dp0build_%MODE%
 
 echo === Configure (%MODE%) ===
 REM Pythonからpybind11のCMakeディレクトリを動的に取得する
 FOR /F "tokens=*" %%i IN ('python -c "import pybind11; print(pybind11.get_cmake_dir().replace('\\', '/'))"') DO set PYBIND11_CMAKE_DIR=%%i
 
-cmake -S . -B %BUILD_DIR% ^
+cmake -S "%~dp0." -B "%BUILD_DIR%" ^
     -Dpybind11_DIR="%PYBIND11_CMAKE_DIR%"
 
 echo === Build (%MODE%) ===
-cmake --build %BUILD_DIR% --config %MODE% --parallel
+cmake --build "%BUILD_DIR%" --config %MODE% --parallel
 
 echo === Done (%MODE%) ===
 
