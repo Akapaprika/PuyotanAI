a2bc2711d1f04a6e1c4269e137ddb7e7e64d7747
Akapaprika <oxygen149656@gmail.com>
2026-08-16 11:26:17 +0900

    build: clang対応

--- native/build.bat
+++ native/build.bat
@@ -1,27 +1,33 @@
 @echo off
 setlocal
 
-REM =====================================
-REM Build script for PuyotanNative
-REM Usage:
-REM   build.bat        -> Release build
-REM   build.bat -d     -> Debug build
-REM =====================================
-
 set MODE=Release
+set TOOLSET=-T ClangCL
 
 if "%1"=="-d" set MODE=Debug
+if "%1"=="-msvc" set TOOLSET=
+if "%1"=="--msvc" set TOOLSET=
+if "%1"=="-clang" set TOOLSET=-T ClangCL
+if "%1"=="--clang" set TOOLSET=-T ClangCL
 
-echo Build mode: %MODE%
+if "%2"=="-d" set MODE=Debug
+if "%2"=="-msvc" set TOOLSET=
+if "%2"=="--msvc" set TOOLSET=
+if "%2"=="-clang" set TOOLSET=-T ClangCL
+if "%2"=="--clang" set TOOLSET=-T ClangCL
+
+echo [Build] Mode: %MODE%, Toolset: %TOOLSET%
 
 set BUILD_DIR=%~dp0build_%MODE%
 
 echo === Configure (%MODE%) ===
-REM Pythonからpybind11のCMakeディレクトリを動的に取得する
 FOR /F "tokens=*" %%i IN ('python -c "import pybind11; print(pybind11.get_cmake_dir().replace('\\', '/'))"') DO set PYBIND11_CMAKE_DIR=%%i
 
-cmake -S "%~dp0." -B "%BUILD_DIR%" ^
-    -Dpybind11_DIR="%PYBIND11_CMAKE_DIR%"
+if "%TOOLSET%"=="" (
+    cmake -S "%~dp0." -B "%BUILD_DIR%" -Dpybind11_DIR="%PYBIND11_CMAKE_DIR%"
+) else (
+    cmake -S "%~dp0." -B "%BUILD_DIR%" %TOOLSET% -Dpybind11_DIR="%PYBIND11_CMAKE_DIR%"
+)
 
 echo === Build (%MODE%) ===
 cmake --build "%BUILD_DIR%" --config %MODE% --parallel
